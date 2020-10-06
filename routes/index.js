e1cf6155d343c2fa181cfd734c6407819ea6273d
William Goode <wgoode3@gmail.com>
2020-10-06 14:48:54 -0400

    initial

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var fs = require("fs");
+var path = require("path");
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  fs.readFile(path.join(__dirname, "../products.json"), (err, data) => {
+    res.render('index', { title: 'Demo Store', products: JSON.parse(data) });
+  });
+});
+
+router.get("/products", (req, res, next) => {
+  fs.readFile(path.join(__dirname, "../products.json"), (err, data) => {
+    res.json(JSON.parse(data));
+  });
+});
+
+module.exports = router;
